35a58a467ce2446016c13a153f9d04d28da28c1a
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-12 19:37:01 -0500

    Finished Lab 09

--- Lab 09 - 10 November/server.R
+++ Lab 09 - 10 November/server.R
@@ -10,14 +10,20 @@ shinyServer(function(input, output) {
     
     colfunc<-colorRampPalette(c("red","royalblue"))
     
-    mydata$color <- 0
-    firstLim <- (currentData$year - mydata$year[1])/3
-    secondLim <- ((currentData$year - mydata$year[1])*2)/3
-
-    mydata$color[(currentData$year - mydata$year) >= firstLim  ] = .1
-    mydata$color[(currentData$year - mydata$year) > firstLim && (currentData$year - mydata$year) <= secondLim  ] = .3
-    mydata$color[(currentData$year - mydata$year) > secondLim && (currentData$year - mydata$year) < secondLim ] = .5
+    #The following is my code to change the alpha level according to the distance of the year, but I thought it was too distracting
+    #so I decided to just keep every year before the current year to be .1 
+    #mydata$color <- 0
+    #maxLim <- currentData$year - mydata$year[1]
+    #secondLim <- maxLim * (2/3)
+    #firstLim <- maxLim * (1/3)
+    #mydata$color[(currentData$year - mydata$year) > 0 && (currentData$year - mydata$year) <= firstLim  ] = .5
+    #mydata$color[(currentData$year - mydata$year) > firstLim && (currentData$year - mydata$year) <= secondLim  ] = .3
+    #mydata$color[(currentData$year - mydata$year) > secondLim && (currentData$year - mydata$year) <= maxLim ] = .1
+    #mydata$color[currentData$year < mydata$year] = 0
+    #mydata$color[mydata$year == currentData$year]= 1
     
+    mydata$color <- 0
+    mydata$color[currentData$year > mydata$year] = .1
     mydata$color[mydata$year == currentData$year]= 1
     
     ggplot(mydata,aes(month,temp, group=year,col=avgTemp))+ labs(x="Months", y="Temperature in Degrees") +
